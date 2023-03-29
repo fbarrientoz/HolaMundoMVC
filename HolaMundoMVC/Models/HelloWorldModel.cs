fdca1694f9e4559c699920c48296cb3679e962f8
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2023-03-28 19:24:25 -0700

    Add project files.

---
+++ HolaMundoMVC/Models/HelloWorldModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolaMundoMVC.Models
+{
+    public class HelloWorldModel
+    {
+        public string Message { get; set; }
+    }
+}
